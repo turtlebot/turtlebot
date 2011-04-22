9edd98fd3bce6525d24c2509bb29ef0dad4b1454
Tony Pratkanis <none@none>
2011-04-22 10:54:51 -0700

    Clean up and document the follower.

--- turtlebot_follower/src/follower.cpp
+++ turtlebot_follower/src/follower.cpp
@@ -1,3 +1,37 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Garratt Gallagher
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name Garratt Gallagher nor the names of other
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
 #include "ros/ros.h"
 #include "pluginlib/class_list_macros.h" 
 #include "nodelet/nodelet.h" 
@@ -6,87 +40,124 @@
 #include <pcl/point_types.h>
 
 
-namespace turtlebot_follower {
-  typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+namespace turtlebot_follower
+{
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-  class TurtlebotFollower : public nodelet::Nodelet 
+//* The turtlebot follower nodelet.
+/**
+ * The turtlebot follower nodelet. Subscribes to point clouds
+ * from the kinect, processes them, and publishes command vel
+ * messages.
+ */
+class TurtlebotFollower : public nodelet::Nodelet 
+{
+public:
+  /*!
+   * @brief The constructor for the follower.
+   * Constructor for the follower.
+   */
+  TurtlebotFollower() : min_y_(0.1), max_y_(0.5),
+                        min_x_(-0.2), max_x_(0.2),
+                        max_z_(0.8), goal_z_(0.6),
+                        z_scale_(1.0), x_scale_(5.0) 
   {
-  public:
-    TurtlebotFollower() : min_y_(0.1), max_y_(0.5),
-			  min_x_(-0.2), max_x_(0.2),
-			  max_z_(0.8), goal_z_(0.6),
-			  z_scale_(1.0), x_scale_(5.0) {
-
+    
+  }
+  
+private:
+  double min_y_; /**< The minimum y position of the points in the box. */
+  double max_y_; /**< The maximum y position of the points in the box. */
+  double min_x_; /**< The minimum x position of the points in the box. */
+  double max_x_; /**< The maximum x position of the points in the box. */
+  double max_z_; /**< The maximum z position of the points in the box. */
+  double goal_z_; /**< The distance away from the robot to hold the centroid */
+  double z_scale_; /**< The scaling factor for translational robot speed */
+  double x_scale_; /**< The scaling factor for rotational robot speed */
+  
+  /*!
+   * @brief OnInit method from node handle.
+   * OnInit method from node handle. Sets up the parameters
+   * and topics.
+   */
+  virtual void onInit() 
+  {
+    ros::NodeHandle& nh = getNodeHandle();
+    ros::NodeHandle& private_nh = getPrivateNodeHandle();
+    
+    private_nh.getParam("min_y", min_y_);
+    private_nh.getParam("max_y", max_y_);
+    private_nh.getParam("min_x", min_x_);
+    private_nh.getParam("max_z", max_z_);
+    private_nh.getParam("goal_z", goal_z_);
+    private_nh.getParam("z_scale", z_scale_);
+    private_nh.getParam("x_scale", x_scale_);
+    
+    cmdpub_ = nh.advertise<geometry_msgs::Twist> ("turtlebot_node/cmd_vel", 1);
+    sub_= nh.subscribe<PointCloud>("/camera/depth/points", 1, &TurtlebotFollower::cloudcb, this);
+  }
+  
+  /*!
+   * @brief Callback for point clouds.
+   * Callback for point clouds. Uses PCL to find the centroid
+   * of the points in a box in the center of the point cloud.
+   * Publishes cmd_vel messages with the goal from the cloud.
+   * @param cloud The point cloud message.
+   */
+  void cloudcb(const PointCloud::ConstPtr&  cloud)
+  {
+    //X,Y,Z of the centroid
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+    //Number of points observed
+    unsigned int n = 0;
+    //Iterate through all the points in the region and find the average of the position
+    BOOST_FOREACH (const pcl::PointXYZ& pt, cloud->points)
+    {
+      //First, ensure that the point's position is valid. This must be done in a seperate
+      //if because we do not want to perform comparison on a nan value.
+      if (!std::isnan(x) && !std::isnan(y) && !std::isnan(z))
+      {
+        //Test to ensure the point is within the aceptable box.
+        if (-pt.y > min_y_ && -pt.y < max_y_ && pt.x < max_x_ && pt.x > min_x_ && pt.z < max_z_)
+        {
+          //Add the point to the totals
+          x += pt.x;
+          y += pt.y;
+          z += pt.z;
+          n++;
+        }
+      }
     }
-
-  private:
-    double min_y_, max_y_, min_x_, max_x_;
-    double max_z_, goal_z_, z_scale_, x_scale_;
-
-    virtual void onInit() {
-      ros::NodeHandle& nh = getNodeHandle();
-      ros::NodeHandle& private_nh = getPrivateNodeHandle();
-
-      private_nh.getParam("min_y", min_y_);
-      private_nh.getParam("max_y", max_y_);
-      private_nh.getParam("min_x", min_x_);
-      private_nh.getParam("max_z", max_z_);
-      private_nh.getParam("goal_z", goal_z_);
-      private_nh.getParam("z_scale", z_scale_);
-      private_nh.getParam("x_scale", x_scale_);
+    
+    //If there are points, find the centroid and calculate the command goal.
+    //If there are no points, simply publish a stop goal.
+    if (n)
+    { 
+      x /= n; 
+      y /= n; 
+      z /= n;  
       
-      cmdpub_ = nh.advertise<geometry_msgs::Twist> ("turtlebot_node/cmd_vel", 1);
-      sub_= nh.subscribe<PointCloud>("/camera/depth/points", 1, &TurtlebotFollower::cloudcb, this);
-
+      ROS_DEBUG("Centriod at %f %f %f with %d points", x, y, z, n);
+      
+      geometry_msgs::Twist cmd;
+      cmd.linear.x = (z - goal_z_) * z_scale_;
+      cmd.angular.z = -x * x_scale_;
+      cmdpub_.publish(cmd);
     }
-
-    void cloudcb(const PointCloud::ConstPtr&  cloud){
-      double x = 0.0;
-      double y = 0.0;
-      double z = 0.0;
-      unsigned int n = 0;
-      BOOST_FOREACH (const pcl::PointXYZ& pt, cloud->points) {
-
-	if ( !std::isnan(x) && !std::isnan(y) && !std::isnan(z) )  {
-	  //printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
-	  if (-pt.y > min_y_ && -pt.y < max_y_) {
-	    if (pt.x < max_x_ && pt.x > min_x_) {
-	      if (pt.z < max_z_) {
-		x += pt.x;
-		y += pt.y;
-		z += pt.z;
-		n++;
-	      }
-	    }
-	  }
-	}
-      }
-      if (n) { 
-	x /= n; y /= n; z /= n;  
-
-	//printf("%f %f %f %d\n", x, y, z, n);
-
-	geometry_msgs::Twist cmd;
-	
-	if (n) {
-	  cmd.linear.x = (z - goal_z_) * z_scale_;
-	  cmd.angular.z = -x * x_scale_;
-	  cmdpub_.publish(cmd);
-	  
-	}
-      } else {
-	ROS_INFO("No object");
-	cmdpub_.publish(geometry_msgs::Twist());
-      }
-
-
+    else
+    {
+      ROS_DEBUG("No points detected, stopping the robot");
+      cmdpub_.publish(geometry_msgs::Twist());
     }
+  }
+  
+  
+  ros::Subscriber sub_;
+  ros::Publisher cmdpub_;
+};
 
-
-    ros::Subscriber sub_;
-    ros::Publisher cmdpub_;
-  };
-
-  PLUGINLIB_DECLARE_CLASS(turtlebot_follower, TurtlebotFollower, turtlebot_follower::TurtlebotFollower, nodelet::Nodelet); 
+PLUGINLIB_DECLARE_CLASS(turtlebot_follower, TurtlebotFollower, turtlebot_follower::TurtlebotFollower, nodelet::Nodelet);
 
 }
