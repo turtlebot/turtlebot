4b38a75323c0be3bcb46e588d56dbef329e80dfd
Dave Hershberger <hersh@willowgarage.com>
2011-05-18 11:30:27 -0700

    new version of turtlebot_bringup/upstart files.  Now you specify at install time which network interface you want to use.  My previous attempt at an automagic system was not able to do the right thing reliably.  See turtlebot_bringup/upstart/README for details.

--- turtlebot_bringup/upstart/install.bash
+++ turtlebot_bringup/upstart/install.bash
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 # Run this as root, from the directory containing it!
+#
+# USAGE: sudo ./install.bash
+#  or
+# sudo ./install.bash usb0
+#
+# where usb0 is whatever network interface you want to set the robot
+# up for.  wlan0 is the default.
 
-cp turtlebot-start turtlebot-stop /usr/sbin
-cp turtlebot*.conf /etc/init
+interface=wlan0
+
+if [ $# -gt 0 ]; then
+    if [ "$1" != "" ]; then
+        interface=$1
+    fi
+fi
+
+echo "Installing using network interface $interface."
+
+sed "s/wlan0/$interface/g" < turtlebot-start > /usr/sbin/turtlebot-start
+sed "s/wlan0/$interface/g" < turtlebot-stop > /usr/sbin/turtlebot-stop
+sed "s/wlan0/$interface/g" < turtlebot.conf > /etc/init/turtlebot.conf
