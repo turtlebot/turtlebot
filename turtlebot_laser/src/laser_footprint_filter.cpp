65c78d21648cdf726faee1b3cd66d53200d2e860
Helen Oleynikova <eoleynikova@willowgarage.com>
2011-07-07 12:58:47 -0700

    turtlebot_laser: works
    --HG--
    branch : interactive_markers

--- turtlebot_laser/src/laser_footprint_filter.cpp
+++ turtlebot_laser/src/laser_footprint_filter.cpp
@@ -39,18 +39,33 @@
 #include <laser_geometry/laser_geometry.h>
 
 #include <pcl/point_cloud.h>
-#include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl/ros/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <pcl_ros/impl/transforms.hpp>
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+struct PointXYZIndex
+{
+  PCL_ADD_POINT4D;                  // preferred way of adding a XYZ+padding
+  int32_t index;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW   // make sure our new allocators are aligned
+} EIGEN_ALIGN16;                    // enforce SSE padding for correct memory alignment
+
+POINT_CLOUD_REGISTER_POINT_STRUCT (PointXYZIndex,           // here we assume a XYZ + "test" (as fields)
+                                  (float, x, x)
+                                  (float, y, y)
+                                  (float, z, z)
+                                  (int32_t, index, index)
+)
+
+typedef pcl::PointCloud<PointXYZIndex> PointCloud;
 
 class LaserFootprintFilter
 {
 public:
   LaserFootprintFilter()
-    : tf_(ros::Duration(10000000))
+    : tf_(ros::Duration(10000000)), first_(true)
   {
     scan_filtered_pub_ = nh_.advertise<sensor_msgs::LaserScan>("/scan_filtered", 1);
     scan_sub_ = nh_.subscribe("/scan", 1000, &LaserFootprintFilter::update, this);
@@ -75,16 +90,24 @@ public:
     // Get the pointcloud from the laser scan
     projector_.projectLaser(input_scan, cloud_message);
     
+    debug_pub_.publish(cloud_message);
     pcl::fromROSMsg(cloud_message, laser_cloud_in);
-    // Get the transform from tf_listener
-    // To do: make this just execute the first time.
-    /*try 
+    
+    /* if (first_)
     {
-       tf_.waitForTransform ("/base_link", input_scan.header.frame_id, input_scan.header.stamp, ros::Duration(0.01));
-    }
-    catch(tf::TransformException& ex){
-      ROS_ERROR("Transform unavailable %s", ex.what());
-    }*/
+      // Get the transform from tf_listener
+      // To do: make this just execute the first time.
+      try 
+      {
+         tf_.waitForTransform ("/base_link", input_scan.header.frame_id, input_scan.header.stamp, ros::Duration(10));
+      }
+      catch(tf::TransformException& ex){
+        ROS_ERROR("Transform unavailable %s", ex.what());
+      }
+      ROS_INFO("Acquired laser transform.");
+      first_ = false;
+      
+    } */
 
     // Not 100% sure what fixed_frame should be.
     pcl_ros::transformPointCloud ("/base_link", input_scan.header.stamp, laser_cloud_in, "/base_link", laser_cloud, tf_);
@@ -96,56 +119,18 @@ public:
     {
       if (inFootprint(laser_cloud.points[i]))
       {
-        //int index = laser_cloud.channels[c_idx].values[i];
-        ROS_INFO("Index: %d, range: %f, x: %f, y: %f, z: %f", i, filtered_scan.ranges[i], laser_cloud.points[i].x,  laser_cloud.points[i].y, laser_cloud.points[i].z);
-        filtered_scan.ranges[i] = filtered_scan.range_max + 1.0; // If so, then make it a value bigger than the max range
+        int index = laser_cloud.points[i].index;
+        filtered_scan.ranges[index] = filtered_scan.range_max + 1.0; // If so, then make it a value bigger than the max range
       }
     }
-    
-    debug_pub_.publish(laser_cloud);
-
-    /* try{
-    projector_.transformLaserScanToPointCloud("/base_link", input_scan, laser_cloud, tf_);
-    }
-    catch(tf::TransformException& ex){
-      ROS_ERROR("Transform unavailable %s", ex.what());
-    } */
-
-    /*int c_idx = indexChannel(laser_cloud);
-
-    if (c_idx == -1 || laser_cloud.channels[c_idx].values.size () == 0)
-    {
-      ROS_ERROR("We need an index channel to be able to filter out the footprint");
-    }
-    
-    for (unsigned int i=0; i < laser_cloud.points.size(); i++)  
-    {
-      if (inFootprint(laser_cloud.points[i])){
-        int index = laser_cloud.channels[c_idx].values[i];
-        filtered_scan.ranges[index] = filtered_scan.range_max + 1.0 ; // If so, then make it a value bigger than the max range
-      }
-    } */
     
     scan_filtered_pub_.publish(filtered_scan);
   }
 
-  int indexChannel(const sensor_msgs::PointCloud& scan_cloud)
-  {
-      int c_idx = -1;
-      for (unsigned int d = 0; d < scan_cloud.channels.size (); d++)
-      {
-        if (scan_cloud.channels[d].name == "index")
-        {
-          c_idx = d;
-          break;
-        }
-      }
-      return c_idx;
-  }
 
   // To do: Currently removes everything in a box: since our robot is circular,
   // change this to be a circle instead.
-  bool inFootprint(const pcl::PointXYZ& scan_pt)
+  bool inFootprint(const PointXYZIndex& scan_pt)
   {
     if(scan_pt.x < -1.0 * inscribed_radius_ || scan_pt.x > inscribed_radius_ || scan_pt.y < -1.0 * inscribed_radius_ || scan_pt.y > inscribed_radius_)
       return false;
@@ -160,6 +145,7 @@ private:
   ros::Publisher debug_pub_;
   ros::Subscriber scan_sub_;
   ros::NodeHandle nh_;
+  bool first_;
 };
 
 int main(int argc, char** argv)
