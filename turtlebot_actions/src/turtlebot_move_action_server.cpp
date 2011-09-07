002002dec482f4fee06a5f7401d5af221a5bafb5
Helen Oleynikova <none@none>
2011-09-07 11:03:55 -0700

    Minor changes to move action server.

--- turtlebot_actions/src/turtlebot_move_action_server.cpp
+++ turtlebot_actions/src/turtlebot_move_action_server.cpp
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
 #include <actionlib/server/simple_action_server.h>
@@ -18,12 +48,13 @@ private:
   ros::NodeHandle nh_;
   actionlib::SimpleActionServer<turtlebot_actions::TurtlebotMoveAction> as_;
   std::string action_name_;
-  turtlebot_actions::TurtlebotMoveFeedback feedback_;
-  turtlebot_actions::TurtlebotMoveResult result_;
+
+  turtlebot_actions::TurtlebotMoveFeedback     feedback_;
+  turtlebot_actions::TurtlebotMoveResult       result_;
   turtlebot_actions::TurtlebotMoveGoalConstPtr goal_;
-  ros::Subscriber sub_;
   
-  ros::Publisher cmd_vel_pub_;
+  ros::Subscriber       sub_;
+  ros::Publisher        cmd_vel_pub_;
   tf::TransformListener listener_;
   
 public:
