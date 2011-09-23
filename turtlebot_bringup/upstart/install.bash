fc31152a857108303b948a49c719fb36cb502d1f
Michael Ferguson <mferguson@willowgarage.com>
2011-09-23 16:23:42 -0700

    make upstart executable, add default setup.bash

--- turtlebot_bringup/upstart/install.bash
+++ turtlebot_bringup/upstart/install.bash
@@ -20,10 +20,15 @@ fi
 echo "Installing using network interface $interface."
 
 sed "s/wlan0/$interface/g" < turtlebot-start > /usr/sbin/turtlebot-start
+chmod +x /usr/bin/turtlebot-start
 sed "s/wlan0/$interface/g" < turtlebot-stop > /usr/sbin/turtlebot-stop
+chmod +x /usr/bin/turtlebot-stop
 sed "s/wlan0/$interface/g" < turtlebot.conf > /etc/init/turtlebot.conf
 
 # Copy files into /etc/ros/electric/turtlebot
 mkdir /etc/ros
 mkdir /etc/ros/electric
-cat turtlebot.launch > /etc/ros/electric/turtlebot.launch 
+cat turtlebot.launch > /etc/ros/electric/turtlebot.launch
+
+echo '. /opt/ros/electric/setup.bash; export ROS_PACKAGE_PATH=/home/turtlebot/ros:${ROS_PACKAGE_PATH}' > /etc/ros/setup.bash
+
