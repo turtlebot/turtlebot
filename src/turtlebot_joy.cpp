9f16be17d35d82651c2cac0c7dc2a942c74cf588
Daniel Stonier <d.stonier@gmail.com>
2012-12-30 21:11:22 +0900

    making use of a centralised mux.

--- src/turtlebot_joy.cpp
+++ src/turtlebot_joy.cpp
@@ -71,7 +71,7 @@ TurtlebotTeleop::TurtlebotTeleop():
   ph_.param("scale_angular", a_scale_, a_scale_);
   ph_.param("scale_linear", l_scale_, l_scale_);
 
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+  vel_pub_ = ph_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TurtlebotTeleop::joyCallback, this);
 
   timer_ = nh_.createTimer(ros::Duration(0.1), boost::bind(&TurtlebotTeleop::publish, this));
