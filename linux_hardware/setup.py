64b687db002d36db7a24ece7037e10d8fa02b995
Jihoon Lee <jihoonlee.in@gmail.com>
2013-03-04 18:55:13 +0900

    error fixes

--- linux_hardware/setup.py
+++ linux_hardware/setup.py
@@ -4,8 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['linux_hardware'],
-    package_dir={'':'src'},
     scripts=['scripts/laptop_battery.py'],
     requires=['diagnostic_msgs','rospy','std_msgs']
 )
