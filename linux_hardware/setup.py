8d6a0a7d303a531ff3f022ddae24bfea5f37b888
Jihoon Lee <jihoonlee.in@gmail.com>
2013-03-05 09:11:12 +0900

    removing global python module setting

--- linux_hardware/setup.py
+++ linux_hardware/setup.py
@@ -4,8 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    scripts=['scripts/laptop_battery.py'],
-    requires=['diagnostic_msgs','rospy','std_msgs']
 )
 
 setup(**d)
--- turtlebot_app_manager/setup.py
+++ turtlebot_app_manager/setup.py
@@ -6,11 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['turtlebot_app_manager'],
     package_dir={'':'src'},
-    scripts=['scripts/app_manager',
-             'scripts/appmaster',
-             'scripts/rosget',
-             'scripts/test_app.py'
-            ],
     requires=['rospy','rosgraph','roslaunch','rosunit']
 )
 
